57a661c4fcfa843cf8fbf5ab762f816660b09c29
Абдрашитов Тимур Ильнурович <22051@iat.iat>
2025-04-30 15:11:14 +0800

    first commit

---
+++ resources/views/glavnaya.blade.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1 - Основа</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alumni+Sans:ital,wght@0,100..900;1,100..900&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <style>
+        @import url(https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap);
+        @import url(https://fonts.googleapis.com/css2?family=Alumni+Sans:ital,wght@0,100..900;1,100..900&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap);
+    </style>
+    @vite('resources/css/Osnova.css')
+</head>
+<body>
+    <div class="a"></div>
+    <div class="q">
+        <div style="height:3000px"></div>
+    <a href="#" title="Вернуться к началу страницы" class="topNubex">	&uarr;  </a>
+        <div class="ima2"></div>
+    <div class="conti1">
+        <span><a href="#section1">Изделия</a></span>
+        <span>Новые релизы</span>
+        <span>Подарки</span>
+    </div>
+    <a href="#"><div class="ima3"></div></a>
+    <input type="search" name="ser">
+    <div class="conti5">
+        <div style="background-image: url(../img/HEADER\ HEART\ REAL.png); width: 22px; height: 22px; background-repeat: no-repeat;" class="hu"></div>
+        <div style="background-image: url(../img/EMPTY\ BAG.png); width: 21px; height: 21px;" class="hu"></div>
+        <div style="background-image: url(../img/bi_person.png); width: 22px; height: 22px;" class="hu"></div>
+    </div>
+</div>
+    <div class="w">
+        &nbsp; <div class="er">
+            <span style="font-size: 45px; font-weight: 600;">Осенняя распродажа</span><br><br>
+            <span style="font-size: 25px; font-weight: 400;">Наступила осень.</span><br>
+            <span style="font-size: 25px; font-weight: 400;">Покупайте наши новые изделия, 
+                начиная с сегодняшнего дня.</span>
+        </div>
+       <div class="et">
+        <a style="color: white;" href="#section1"><span>Купить сейчас</span></a>
+        </div>
+</div>
+ <div class="e" id="section1">
+        <div class="conti2">
+    <span style="font-size: 30px;">Делайте покупки по категориям</span><br>
+    <span style="padding-left: 45px; font-size: 20px;">Насладитесь тем, что мы предлагаем.</span>
+        </div>
+        <div class="conti3">
+            <div style="background-image: url(../img/Rectangle\ 38\ \(5\).png);" class="hi">
+                <div style="width: 130px; border: 1px solid black; margin-left: 55px; border-radius: 8px;" class="hara"><a href="#">Ожерелья</a></div>
+            </div>
+            <div style="background-image: url(../img/Rectangle\ 38\ \(6\).png);" class="hi">
+                <div style="width: 130px; border: 1px solid black; margin-left: 55px; border-radius: 8px;" class="hara"><a href="#">Серьги</a></div>
+            </div>
+            <div style="background-image: url(../img/Rectangle\ 38\ \(7\).png);" class="hi">
+                <div style="width: 130px; border: 1px solid black; margin-left: 55px; border-radius: 8px;" class="hara"><a href="#">Браслеты</a></div>
+            </div>
+            <div style="background-image: url(../img/Rectangle\ 38\ \(8\).png);" class="hi">  
+                <div style="width: 130px; border: 1px solid black; margin-left: 55px; border-radius: 8px;" class="hara"><a href="#">Кольца</a></div>
+            </div>
+            <div style="background-image: url(../img/Rectangle\ 24\ \(1\).png);" class="hi"> 
+                <div style="width: 130px; border: 1px solid black; margin-left: 55px; border-radius: 8px;" class="hara"><a href="#">Чары</a></div>
+            </div>
+        </div>
+</div>
+    <div class="r">
+        <div class="poer"></div>
+        <div class="conti4">
+        <span style="font-size: 20px;">Артикль    &nbsp;   •  &nbsp;   &nbsp; Апрель  &nbsp;   •  &nbsp; &nbsp; 2025</span><br><br><br><br><br>
+        <span style="font-size: 35px;">В золотой час.</span><br><br>
+        <span style="font-size: 28px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br> 
+            tempor incididunt ut labore et dolore magna aliqua. Metus vulputate eu <br>
+            scelerisque felis imperdiet proin fermentum. Cras semper auctor neque <br>
+            vitae tempus quam pellentesque. Elementum sagittis vitae et leo duis.<br><br>
+
+            Elementum sagittis vitae et leo duis. Libero nunc consequat interdum <br>
+            varius. Habitant morbi tristique senectus et netus et malesuada fames ac. </span>
+        </div>
+        <div class="ert"><a style="color: white;" href="../Index/4.html">Читать</a></div>
+</div>
+    <div class="t">
+        &nbsp;   <div class="conti6">
+    <span style="font-size: 45px;">Подарки сезона</span><br><br>
+    <span style="font-size: 23px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br>
+    do eiusmod tempor incididunt ut labore et dolore magna <br>
+    aliqua. Metus vulputate eu scelerisque felis imperdiet proin <br>
+    fermentum. Cras semper auctor neque vitae tempus quam <br>
+    pellentesque. Elementum sagittis vitae et leo duis.
+</span>
+</div>
+<div class="rty"><a href="../Index/gifts.html">Купить подарки</a></div>
+<div class="ima4"></div>
+</div>
+    <div class="y">
+<div class="ima5"></div>
+<div class="conti7">
+    <span style="font-size: 35px;">Кто мы такие?</span><br><br><br>
+    <span style="font-size: 25px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br>
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. <br>
+        Metus vulputate eu scelerisque felis imperdiet proin <br>
+        fermentum. Cras semper auctor neque vitae tempus quam <br>
+        pellentesque. Elementum sagittis vitae et leo duis.<br><br>
+
+        Elementum sagittis vitae et leo duis. Libero nunc consequat <br>
+        interdum varius. Habitant morbi tristique senectus et netus et <br>
+        malesuada fames ac. 
+    </span>
+</div>
+<div class="rti"><a style="color: white;" href="../Index/about us.html">О нас</a></div>
+</div>
+<div class="footer">
+    &nbsp;   <div class="contiall">
+        <div class="conti8">
+            <span style="font-size: 20px; font-weight: 600;">Обслуживание клиентов</span><br><br>
+            <span style="font-size: 18px;">Отследить заказ</span><br><br>
+            <span style="font-size: 18px;">Доставка и возврат</span><br><br>
+            <span style="font-size: 18px;">Связь с нами</span><br><br>
+        </div>
+        <div class="conti9">
+            <span style="font-size: 20px; font-weight: 600;"> О нас</span><br><br>
+            <span style="font-size: 18px;">Наша цель</span><br><br>
+            <span style="font-size: 18px;">Карьера</span><br><br>
+            <span style="font-size: 18px;">Контакты</span><br><br>
+        </div>
+        <div class="conti10">
+            <span style="font-size: 20px; font-weight: 600;">Забота о материалах</span><br><br>
+            <span style="font-size: 18px;">Ремонт изделия</span><br><br>
+            <span style="font-size: 18px;">Размеры</span><br><br>
+            <span style="font-size: 18px;">Советы</span><br><br>
+        </div>
+        <div class="conti11">
+            <span style="font-size: 20px; font-weight: 600;">Основные офисы</span><br><br>
+            <span style="font-size: 18px;">Иркутск, RU</span><br><br>
+            <span style="font-size: 18px;">Москва, RU</span><br><br>
+            <span style="font-size: 18px;">Оренбург, RU</span><br><br>
+        </div>
+    </div>
+<div class="yu">
+    <span style="font-size: 20px; font-weight: 600; color: black;">Вы можете быть на шаг впереди.</span><br><br>
+    <span style="font-size: 18px;">Подпишитесь, чтобы получать информацию о наших обновлениях.</span>
+</div>
+<input type="email" name="emae">
+<div class="si"><a href="#">Sign up</a></div>
+<div class="ima7"></div>
+<div class="kam">KAMENIKA, LLC</div>
+<div class="conti12">
+    <span>PRIVACY POLICY </span>&nbsp;  • &nbsp; 
+    <span>TERMS OF USE </span>&nbsp;  • &nbsp; 
+    <span>SITEMAP</span>&nbsp;  • &nbsp; 
+    <span>DO NOT SELL MY INFORMATION</span>&nbsp;  • &nbsp; 
+    <span>COOKIES</span>&nbsp;  &nbsp; 
+</div>
+</div>
+</body>
+</html>
